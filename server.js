3c698a8d35eb593949b34f4f885e45a7efed5a1e
daisy77125 <32116413+daisy77125@users.noreply.github.com>
2022-01-01 14:03:00 -0500

    set up a basic express server that can: 1. read env variables; 2. log income HTTP requests - methods + url; 3. Body Parser Middleware 4. GET /items route returns a simple hello-world string

---
+++ server.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+
+const logger = require("./middleware/logger");
+const itemsRouter = require("./routes/items");
+
+dotenv.config({ path: "./.env" }); // Load config
+
+const app = express();
+
+//#region Middleware
+app.use(cors());
+app.use(logger); // Init logger
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/items", itemsRouter);
+
+//#endregion
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
