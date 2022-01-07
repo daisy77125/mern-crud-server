eec63dabd0f2198646ff8abbdeaf8ad6ee7c2c26
daisy77125 <32116413+daisy77125@users.noreply.github.com>
2022-01-06 19:03:41 -0500

    update server.js: 'hello world'

--- server.js
+++ server.js
@@ -23,5 +23,9 @@ app.use("/posts", postsRouter);
 
 //#endregion
 
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
